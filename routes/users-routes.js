864c303f51e8a1c034bfb4fdf698803ed8edf761
NahyeonKim <parakise13@gmail.com>
2022-08-26 20:51:59 +0900

    file upload

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users-controller");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single('imageUrl'),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
@@ -17,9 +19,6 @@ router.post(
   usersControllers.signup
 );
 
-router.post(
-  "/login",
-  usersControllers.login
-);
+router.post("/login", usersControllers.login);
 
 module.exports = router;
