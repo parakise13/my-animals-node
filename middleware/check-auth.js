5a1c3f833248f6008c61ace91ceeb0882968cab8
NahyeonKim <parakise13@gmail.com>
2022-08-29 21:20:04 +0900

    modify variables

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,19 +3,18 @@ const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
 
 module.exports = (req, res, next) => {
-	if (req.method === 'OPTIONS') {
-		return next();
-	}
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
       throw new Error("인증에 실패했습니다.");
     }
 
-		const decodedToken = jwt.verify(token, 'supersecret_dont_share');
-		req.userData = { userId: decodedToken.userId };
-		next();
-
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    req.userData = { userId: decodedToken.userId };
+    next();
   } catch (err) {
     const error = new HttpError("인증에 실패했습니다.", 403);
     return next(error);
